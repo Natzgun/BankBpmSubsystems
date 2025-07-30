ba2b1d6631aeec3b9d8359ff3342815aaf810db7
4meM <eramosq@unsa.edu.pe>
2025-07-29 22:39:44 -0500

    agregando peticiones post para solicitud de Credito

--- src/main/java/com/hamuksoft/bankbpmsubsytems/modules/GestionSolicitudCredito/application/GestionarSolicitudCreditoImpl.java
+++ src/main/java/com/hamuksoft/bankbpmsubsytems/modules/GestionSolicitudCredito/application/GestionarSolicitudCreditoImpl.java
@@ -1,4 +1,13 @@
 package com.hamuksoft.bankbpmsubsytems.modules.GestionSolicitudCredito.application;
+import com.hamuksoft.bankbpmsubsytems.modules.GestionSolicitudCredito.application.Servicio.IGestionarSolicitudCredito;
+import org.springframework.stereotype.Service;
+import java.util.Map;
 
-public class GestionarSolicitudCreditoImpl {
+@Service
+public class GestionarSolicitudCreditoImpl implements IGestionarSolicitudCredito {
+    @Override
+    public Map<String, Object> evaluarSolicitud(Map<String, Object> datos) {
+        System.out.println("Procesando Solcitud: " + datos);
+        return Map.of("mensaje", "ok", "clienteAprobado", false);
+    }
 }
